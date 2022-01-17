862dd88b33ae7d90e3e16cccf2d288aa0cdbe084
EC2 Default User <ec2-user@ip-172-31-5-94.eu-central-1.compute.internal>
2022-01-17 14:44:45 +0000

    added query for database: get random wisdom and output wisdom on home path

--- backend/app.py
+++ backend/app.py
@@ -9,13 +9,6 @@ connection = psycopg2.connect(dbname = os.environ["DATABASE_NAME"],
                               password = os.environ["DATABASE_PASSWORD"],
                               host = os.environ["DATABASE_HOST"],
                               port = os.environ["DATABASE_PORT"])
-# Open a cursor to perform database operations
-cursor = connection.cursor()
-# Execute a query
-cursor.execute("SELECT * FROM my_data")
-# Retrieve query results
-records = cursor.fetchall()
-
 
 app = Flask(__name__)
 LOG = create_logger(app)
@@ -25,11 +18,27 @@ LOG.setLevel(logging.INFO)
 def home():
     html = f"<h1>GetWise</h1>\n\n"
 
-    # TODO: Get a random wisdom from the db
-    #LOG.info(f"Wisdom: \n{wisdom}")
-    #LOG.info(f"Source: \n{source}")
-    #return jsonify({'Wisdom': wisdom, 'Source': source})
-    return html.format(format) + "<p>Hello, World!</p>\n"
+    # Open a cursor to perform database operations
+    cursor = connection.cursor()
+    # Execute a query
+    cursor.execute("SELECT * FROM Wisdoms ORDER BY random() LIMIT 1")
+    # Retrieve query results
+    records = cursor.fetchall()
+    wisdom = records[0][1]
+    source = records[0][2]
+    # TODO: add query for categories and images
+    cursor.close()
+
+    LOG.info(f"Wisdom: \n{wisdom}")
+    LOG.info(f"Source: \n{source}")
+    # TODO: log categories and image urls
+
+    html += wisdom + "\n\n"
+    if source:
+        html += "source: " + source + "\n\n"
+    # TODO: output categories and image
+
+    return html.format(format)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
