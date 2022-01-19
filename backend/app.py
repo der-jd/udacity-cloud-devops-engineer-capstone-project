70581a1e36c723be3c3fae4419d7bfbe3c637ff1
EC2 Default User <ec2-user@ip-172-31-0-199.eu-central-1.compute.internal>
2022-01-19 16:33:25 +0000

    return result as json instead of html (prepared connection to frontend)

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from flask.logging import create_logger
+import json
 import logging
 import os
 import psycopg2
@@ -36,24 +37,29 @@ def home():
 
     # Query for wisdom categories
     cursor.execute("SELECT category FROM Categories WHERE wisdomId = '" + str(id_) + "'")
-    categories = cursor.fetchall()
-    LOG.info(f"Categories:")
-    if categories:
+    categories_sql = cursor.fetchall()
+    if categories_sql:
         html += f"<p>Categories:</p>\n"
-    for c in categories:
-        LOG.info(f"{c[0]}")
+    categories = []
+    for c in categories_sql:
+        categories.append(c[0])
         html += f"<p>" + c[0] + "</p>\n"
+    LOG.info(f"Categories:")
+    LOG.info(f"{categories}")
 
     # Query for image urls
     cursor.execute("SELECT imageUrl FROM Images WHERE wisdomId = '" + str(id_) + "'")
-    imageUrls = cursor.fetchall()
-    LOG.info(f"Image URLs:")
-    for u in imageUrls:
-        LOG.info(f"{u[0]}")
+    imageUrls_sql = cursor.fetchall()
+    imageUrls = []
+    for u in imageUrls_sql:
+        imageUrls.append(u[0])
         html += f"<img src=\"" + u[0] + "\">\n"
+    LOG.info(f"Image URLs:")
+    LOG.info(f"{imageUrls}")
     cursor.close()
 
-    return html.format(format)
+    #return html.format(format)
+    return jsonify({"wisdom": wisdom, "source": source, "categories": json.dumps(categories), "img-urls": json.dumps(imageUrls)})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
