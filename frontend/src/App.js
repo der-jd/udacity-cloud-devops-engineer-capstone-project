42307aa565e28c296a088a48fddfa2c09a639c7d
EC2 Default User <ec2-user@ip-172-31-4-81.eu-central-1.compute.internal>
2022-01-21 17:23:19 +0000

    pass json result correctly to state object. WIP: display json result in html

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,25 +2,51 @@ import logo from './logo.svg';
 import './App.css';
 import React from "react";
 
+function validImageUrl(url) {
+  if (url) {
+    let tag = document.createElement("img");
+    tag.src = url;
+    tag.alt = "Descriptive img";
+    //document.getElementById("img").appendChild(tag);
+    return tag;
+  }
+}
+
 class App extends React.Component {
 
   constructor(props) {
     super(props);
 
     this.state = {
-      items: [],
-      DataIsLoaded: false
+      wisdom: "",
+      source: "",
+      categories: [],
+      imageUrls: [],
+      dataIsLoaded: false
     };
   }
 
   // https://www.geeksforgeeks.org/how-to-fetch-data-from-an-api-in-reactjs/
   render() {
-    const { DataIsLoaded, items } = this.state;
-    if (!DataIsLoaded)
+    if (!this.state.dataIsLoaded)
       return <div><h1>Pleses wait until data from backend is fetched...</h1></div>;
 
+    console.log("type wisdom: " + typeof this.state.wisdom)
+    console.log("type categories: " + typeof this.state.categories)
+    console.log("type items: " + typeof this.state.items)
+
+    // TODO: Format and output JSON response correctly
+    //<img src={this.state.imageUrls[0]} alt="Descriptive img" />
+    //displayImage(this.state.imageUrls[0])</script>
     return (
       <div className="App">
+        <h1>GetWise</h1>
+        <pre>{this.state.wisdom}</pre>
+        <p>Source: {this.state.source}</p>
+        <p>Categories: {this.state.categories.join(', ')}</p>
+        <div id="img"/>
+        <script>{document.getElementById("img").appendChild(validImageUrl(this.state.imageUrls[0]))}
+        </script>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
@@ -35,11 +61,6 @@ class App extends React.Component {
             Learn React
           </a>
         </header>
-
-        <h1>GetWise</h1>
-        {
-          <p>Data from backend api: {items}</p> // TODO: Format and output JSON response (items) correctly
-        }
       </div>
     );
   }
@@ -53,8 +74,11 @@ class App extends React.Component {
       .then(res => res.json())
       .then(jsonData => {
         this.setState({
-          items: jsonData,
-          DataIsLoaded: true
+          wisdom: jsonData.wisdom,
+          source: jsonData.source,
+          categories: jsonData.categories,
+          imageUrls: jsonData["image-urls"],
+          dataIsLoaded: true
         });
         console.log(jsonData);
       })
