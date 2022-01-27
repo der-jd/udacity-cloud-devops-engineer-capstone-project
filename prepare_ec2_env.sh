4b3f14c327144806a635b2ae2d391b64dd22b8e1
der-jd <non>
2022-01-27 12:56:48 +0100

    added file to simplify setup of environment on an ec2 instance

---
+++ prepare_ec2_env.sh
@@ -0,0 +1,18 @@
+# Run AFTER activating virtual environment
+
+# Resize ec2 storage
+./resize.sh
+
+# Install dependencies
+make install
+sudo make install_hadolint
+
+cd ..
+
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# Install minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
