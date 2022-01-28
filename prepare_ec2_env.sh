b31733803acf59da689eb632d71dfb08cc27da29
EC2 Default User <ec2-user@ip-172-31-9-46.eu-central-1.compute.internal>
2022-01-28 16:28:39 +0000

    remove artifacts from dependency installation

--- prepare_ec2_env.sh
+++ prepare_ec2_env.sh
@@ -12,7 +12,9 @@ cd ..
 # Install kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+rm kubectl
 
 # Install minikube
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
+rm minikube-linux-amd64
