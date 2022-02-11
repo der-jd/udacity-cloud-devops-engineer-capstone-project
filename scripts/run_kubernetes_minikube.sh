76503080a3cfacb96e62b946d8838b3920ea5c78
EC2 Default User <ec2-user@ip-172-31-14-254.eu-central-1.compute.internal>
2022-02-11 13:14:34 +0000

    fixed if condition for not running minikube cluster before starting it. removed unnecessary removal of kubectl config file. increased sleep time before all K8s resources are running.

--- scripts/run_kubernetes_minikube.sh
+++ scripts/run_kubernetes_minikube.sh
@@ -6,12 +6,11 @@
 scriptPath=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd $scriptPath
 
-if minikube status | grep "host: Stopped"; then
+if minikube status | grep -e "\"minikube\" not found" -e "host: Stopped"; then
     minikube start
 fi
 
 # Update kubectl to point to minikube cluster
-rm ~/.kube/config
 minikube update-context
 
 kubectl create secret generic database-access --from-env-file=/etc/environment
@@ -20,7 +19,7 @@ kubectl apply -f ../backend/backend-deployment.yaml
 kubectl apply -f ../backend/backend-service.yaml
 
 # Wait until pods are running
-sleep 10s
+sleep 15s
 
 kubectl get deploy
 kubectl get svc
