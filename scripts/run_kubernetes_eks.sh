aebf3d32c951a071251684aedb7ecd2e5388c221
EC2 Default User <ec2-user@ip-172-31-14-254.eu-central-1.compute.internal>
2022-02-11 13:11:31 +0000

    configure kubectl to point to EKS cluster before creating resources on it. wait time until all resources are running increased.

--- scripts/run_kubernetes_eks.sh
+++ scripts/run_kubernetes_eks.sh
@@ -3,10 +3,13 @@
 # This file runs a K8s service and deployment with the containerized application in the EKS cluster.
 # Assumes that an image is uploaded to DockerHub via `upload_docker.sh`
 
+uuid=1aa940fd-79db-4e9a-9954-ca60c5b021c8
+
 scriptPath=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd $scriptPath
 
-# TODO: config kubectl to access EKS
+# Update kubectl to point to EKS cluster
+aws eks update-kubeconfig --name cluster-$uuid
 
 kubectl create secret generic database-access --from-env-file=/etc/environment
 
@@ -14,7 +17,7 @@ kubectl apply -f ../backend/backend-deployment.yaml
 kubectl apply -f ../backend/backend-service.yaml
 
 # Wait until pods are running
-sleep 10s
+sleep 15s
 
 kubectl get deploy
 kubectl get svc
