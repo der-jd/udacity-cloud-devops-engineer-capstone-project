ede5080c8f7b85f29d2fb7148282b8023af9984c
EC2 Default User <ec2-user@ip-172-31-15-172.eu-central-1.compute.internal>
2022-02-14 16:58:22 +0000

    use password for DockerHub from environment variable if available

--- scripts/upload_docker.sh
+++ scripts/upload_docker.sh
@@ -8,8 +8,13 @@ dockerpath=judt/udacity-cloud-devops-engineer-capstone-project
 
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+
 echo "Login to DockerHub..."
-docker login --username judt
+if [ -z "$DOCKERHUB_PASSWORD" ]; then
+    docker login --username judt
+else
+    echo $DOCKERHUB_PASSWORD | docker login --username judt --password-stdin
+fi
 docker tag udacity-project $dockerpath
 
 # Push image to a docker repository
