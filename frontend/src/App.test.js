e67440d5800046e30e50fe1fd368218db342f8c3
EC2 Default User <ec2-user@ip-172-31-15-172.eu-central-1.compute.internal>
2022-02-14 14:02:01 +0000

    updated/added basic tests for frontend

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('title is set', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = screen.getByText("GetWise");
+  expect(title).toBeInTheDocument();
+});
+
+test('reload button exists', () => {
+  render(<App />);
+  const reloadButton = screen.getByText("Reload");
+  expect(reloadButton).toBeInTheDocument();
 });
